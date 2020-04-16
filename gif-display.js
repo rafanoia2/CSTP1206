c885832ab34091d93276152de23c78584172c9ea
rafanoia2 <59714149+rafanoia2@users.noreply.github.com>
2020-04-16 03:35:54 -0700

    Add files via upload

--- gif-display.js
+++ gif-display.js
@@ -1,7 +1,4 @@
-// This class will represent the gif display area. It keeps track of which gif
-// is being shown and can select a new random gif to be shown.
-// 
-// See HW4 writeup for more hints and details.
+
 class GifDisplay {
   constructor(info, showMusicPage) {
     // TODO(you): Implement the constructor and add fields as necessary.
